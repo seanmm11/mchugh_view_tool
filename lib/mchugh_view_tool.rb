c195218140b8546687e793ff3383d084c7a81beb
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 14:44:02 +0000

    Dev tool initial commit

---
+++ lib/mchugh_view_tool.rb
@@ -0,0 +1,6 @@
+require "mchugh_view_tool/version"
+require "mchugh_view_tool/renderer"
+
+module MchughViewTool
+  # Your code goes here...
+end
---
+++ lib/mchugh_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MchughViewTool
+  VERSION = "0.1.0"
+end
